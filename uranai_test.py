412d893fe2ab2bd78426b6fbe64534e743095cfe
hosokawa Junya <hosokawa@morijyobi.ac.jp>
2020-06-11 17:08:59 +0900

    add pytest　test case

---
+++ uranai_test.py
@@ -0,0 +1,13 @@
+import ex01;
+
+@pytest.mark.parametrize('ans', [
+    "大吉です。最高♪♪♪",
+    "中吉です。ラッキー♪",
+    "小吉です。コツコツと…",
+    "末吉です。焦らずに",
+    "凶です。気をつけて！"
+])
+def test_ex01():
+    assert ex01.uranai() == ans
+    
+    
